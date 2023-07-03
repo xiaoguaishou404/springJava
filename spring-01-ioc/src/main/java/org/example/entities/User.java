197edb3e056f8752cf4c10e2513ddf7d80264a0c
shenshen <xiaoguaishou404@qq.com>
2023-07-03 16:35:04 +0800

    备份2

--- spring-01-ioc/src/main/java/org/example/entities/User.java
+++ spring-01-ioc/src/main/java/org/example/entities/User.java
@@ -4,6 +4,13 @@ public class User {
 
     private String name;
 
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "User{" +
